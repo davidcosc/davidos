eb78eaeb36b4f47e3dc572e246b6dcff62ec99a2
David Cornel <david.cosc@gmx.net>
2023-09-15 15:49:06 +0200

    chore: adjusted comments

--- mbr.asm
+++ mbr.asm
@@ -18,7 +18,7 @@ loop:                                      ; jumping to a label is done using a 
                                            ; The first byte of a SHORT Jump is always EB and the second is a relative offset from 00h to 7Fh for Forward jumps
                                            ; and from 80h to FFh for Reverse (or Backward) jumps https://thestarman.pcministry.com/asm/2bytejumps.htm 
        
-print_string:                              ; this function takes one parameter that must be stored in bx before calling => bx should point to the starting address of a string to be printed
+print_string:                              ; this function takes one parameter that must be stored in bx before calling => bx should point to the starting address of a string
   pusha                                    ; push all current register values to the stack => save the state of all registers before running this function
   mov al, [ds:bx]                          ; move the value stored at the address stored in bx to al (basically dereferencing)
   print_next_char:       
@@ -32,7 +32,7 @@ print_string:                              ; this function takes one parameter t
        
 print_hex_rm:                              ; this function takes a hex number as parameter that must be stored in dx before calling 
   pusha       
-  mov cx, 4                                ; we gonna work on all of the 4 nibbles contained in dx in sequence => we set our initial counter to 4 to keep track on how many nibbles we already worked on
+  mov cx, 4                                ; we gonna work on all of the 4 nibbles contained in dx in sequence => we set our initial counter to 4 to keep track
   mov al, '0'                              ; print '0x' prefix before printing the char converted hex number
   call print_char       
   mov al, 'x'       
@@ -42,22 +42,22 @@ print_hex_rm:                              ; this function takes a hex number as
     shr bl, 4                              ; since the smallest unit we can copy between registers is a byte e.g 9a, we shift to the right by 4 to isolate the left most nibble e.g. 9
     mov al, bl                             ; we copy the nibble to al for printing e.g. this results in al containing 0x09
     call print_hex_nibble       
-    shl dx, 4                              ; we now shift dx to the left by 4 in order to remove the nibble we just printed e.g this results in bx containing 0xabc0, making a the left most nibble
+    shl dx, 4                              ; we now shift dx to the left by 4 in order to remove the nibble we just printed e.g this results in bx containing 0xabc0
     dec cx                                 ; we are finished working on the first nibble, so we reduce our counter by one
     jnz next_nibble                        ; as long as our counter is not zero, we still have nibbles to work on, so we start working on the next nibble
   popa       
   ret       
        
-print_hex_nibble:                          ; this funciton takes one nibble (4 bit value or byte value with its 4 high bits set to zero) as parameter that must be stored in al before calling
+print_hex_nibble:                          ; this funciton takes one nibble (or byte value with its 4 high bits set to zero) as parameter that must be stored in al before calling
   pusha       
   cmp al, 9       
   jle digit       
   letter:       
     sub al, 0xa                            ; in ASCII Code the starting at e.g. the hex code for 'a' incremented by one gets us the hex code for 'b' which incremented by one again 
-    add al, 'a'                            ; gets us the hex code for 'c' and so on, same goes for digits '0' to '9' => in order to print hex numbers greater 0x9 we reduce the number by 0xa
-    jmp print_nibble                       ; => this gives us the offsets for a to f respectively 0 to 5 which we can use to get the hex code / value for the letters 'a' to 'f' by increasing
-  digit:                                   ; the hex code for the letter 'a' by the offset => e.g to print 0xb we get the hex code of 'a' incremented by one (0xb minus ten)
-    add al, '0'                            ; using the same principle we can use an offset as well to calculate hex codes for symbols '0' to '9'
+    add al, 'a'                            ; gets us the hex code for 'c' and so on, same goes for digits '0' to '9' => in order to print hex numbers greater 0x9 we reduce the
+    jmp print_nibble                       ; number by 0xa => this gives us the offsets for a to f respectively 0 to 5 which we can use to get the hex code/value for the
+  digit:                                   ; letters 'a' to 'f' by increasing the hex code for the letter 'a' by the offset => e.g to print 0xb we get the hex code of 'a'
+    add al, '0'                            ; incremented by one (0xb minus ten), also using the same principle we can calculate hex codes for symbols '0' to '9'
   print_nibble:       
     call print_char       
     popa       
