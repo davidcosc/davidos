5b70eaf35c1e3c6d8a5b607a635da4da0f11eec8
David Cornel <david.cosc@gmx.net>
2023-09-16 18:09:54 +0200

    feat: added parts of the gdt data struct, added a separate basics.asm tutorial and moved a lot of documentation/comments of cmds and concepts over there

---
+++ assembly_introduction/basics.asm
@@ -0,0 +1,83 @@
+; this is an miniature bootable example assembly program intended to familiarize us with some nasm assembly syntax,
+; important commands and concepts we are gonna use in our actual mbr
+; we can assemble our program e.g. using the command nasm basics.asm -f bin -o basics.bin
+; and run it using qemu-system-x86_64 basics.bin
+; 
+; instructions in nasm e.g. mov ax, 5 usually follow the structure of <instruction> <target> [, <source>] and translate to so called opcodes
+; opcodes can be used to find our instructions in the generated hex code later e.g. via command od -t x1 -A n basics.bin
+;
+; info about all x86 details can be found at https://redirect.cs.umbc.edu/courses/pub/www/courses/undergraduate/CMPE310/Fall09/cpatel2/nasm/nasmdoca.html#section-A.2.1
+;
+; some terminology:
+;   r/m => register or memory address
+;   /r => effective address encoded in up to three parts: a ModR/M byte, an optional SIB byte, and an optional byte, word or doubleword displacement field
+;   +r => add register number e.g. al=0 ax=0 bl=3, ib or iw => hard coded byte or word value
+;   rb, rw => one of the operands to the instruction is an immediate value/hard coded address
+;          => the difference between this value and the address of the end of the instruction is to be encoded as a byte, word (e.g. backwards ff-distance, forward 00+distance)
+;   ib, iw => immediate/hard coded byte or word
+;
+; for starters we should familiarize us with the following instructions:
+;   mov — move
+;     copies the data item referred to by its second operand (i.e. register contents, memory contents, or a constant value)
+;     into the location referred to by its first operand (i.e. a register or memory)
+;     opcodes of variants we use:
+;       mov r/m8,reg8       88 /r
+;       mov r/m16,reg16     89 /r
+;       mov reg8,r/m8       8a /r
+;       mov reg16,r/m16     8b /r
+;       mov reg8,imm8       b0+r ib
+;       mov reg16,imm16     b8+r iw
+;   jmp — jump
+;     jumps to a given address, the address may be specified as an absolute segment and offset, or as a relative jump within the current segment
+;     opcodes of variants we use:
+;       jmp [SHORT] imm     eb rb
+;   call — call
+;     calls a subroutine, by means of pushing the current instruction pointer (IP) and optionally CS as well on the stack, and then jumping to a given address
+;     opcodes of variants we use:
+;       call imm            e8 rw
+;   pusha — push all
+;     pushes in succession, AX, CX, DX, BX, SP, BP, SI and DI on the stack, decrementing the stack pointer by a total of 16
+;     opcode:
+;       pusha               60
+;   popa — pop all
+;     pops a word from the stack into each of, successively, DI, SI, BP, nothing (it discards a word from the stack which was a placeholder for SP), BX, DX, CX and AX
+;     opcode:
+;       popa                61
+;   ret — return
+;     pop IP from the stack and transfer control to the new address
+;     opcode:
+;       ret                 c3
+;   int — interrupt
+;     causes a software interrupt through a specified vector number from 0 to 255
+;     opcode:
+;       int imm8            cd ib
+[bits 16]                                  ; tell the assembler, that we want our code assembled in 16 bit mode
+main:
+  mov al, 0x99                             ; mov reg8,imm8 => b0 99
+  mov ax, 0xffff                           ; mov reg16,imm16 => b8 ff ff
+  mov bl, al                               ; mov r/m8,reg8 => 88 c3
+  mov bx, ax                               ; mov r/m16,reg16 => 89 c3 
+  mov bl, [dat]                            ; mov reg8,r/m8 => 8a 1e 16 00
+  mov bl, dat                              ; mov reg8,imm8 => b3 16
+  mov al, 'a'                              ; => b0 61
+  call print_char                          ; call imm => e8 05 00
+  jmp loop                                 ; jmp [SHORT] imm => eb 01
+
+dat:
+  db 0xff                                  ; => ff
+
+loop:
+  jmp loop                                 ; => eb fe
+
+print_char:                                ; this function takes one parameter that must be stored in al before calling => the value stored in al will be printed to the screen
+  pusha                                    ; 60
+  mov ah, 0xe                              ; => b4 0e => 0xe in ah refers to the scrolling teletype BIOS routine if int 0x10 is called after
+  int 0x10                                 ; int imm8 => cd 10 => we are in real mode with the interrupt vector already setup by the BIOS and 0x10 referring to the video interrupt
+  popa                                     ; 60
+  ret                                      ; c3
+
+padding:       
+  times 510-(padding-main) db 0x0          ; to make the BIOS recognize this sector as a boot block we must ensure it has a size of exactly 512 bytes
+                                           ; => if the programm size is smaller than 510 bytes we have to pad the remaining bytes with zeros
+       
+dw 0xaa55                                  ; the last 2 bytes must be the magic number 0xaa55 representing endianness
--- mbr.asm
+++ mbr.asm
@@ -1,3 +1,4 @@
+[bits 16]
 BOOT_SECTOR_START_ADDRESS equ 0x7c0        ; by convention the bios loads the mbr to address 0x7c00, for the initial memory setup see ./important_memory_addresses.png
                                            ; in real mode physical addresses are calculated based on the segment address * 16 + offset
                                            ; e.g. if the assembled programm bb c0 07 8e db 8e d3 8e c3 bd 00, was stored at 0x7c00 with the ds segment address set to 0x07c0
@@ -5,21 +6,43 @@ BOOT_SECTOR_START_ADDRESS equ 0x7c0        ; by convention the bios loads the mb
                                            ; or 0x7c00(0x07c0 * 16) with an added offset of 9 since bd is the 10th byte(we start counting from zero)
 main:    
   mov bx, BOOT_SECTOR_START_ADDRESS        ; we can not set segment registers directly, so we store the in another register and then copy it                 
-  mov ds, bx
-  mov ss, bx
+  mov ds, bx                               ; we also do not set the code register since it is only ment to change automatically in combination with the 
+  mov ss, bx                               ; instruction pointer when using a far jump
   mov es, bx                               ; with all segment registers set to 0x7c0 and code loaded to 0x7c00 printing the hex address/offset of our main label should return 0x0000
   mov bp, 0x0000                           ; our stack segment starts at 0x7c00 so we set the stack and base pointer to address 0x0000 to ensure at the beginning
   mov sp, bp                               ; since the stack grows downward we wont overlap with our mbr, but the stack must not grow past 0x500, see ./important_memory_addresses                
   mov bx, welcome_string     
-  call print_string                        ; a call is a jump that additionally pushes the return address (address of the next instruction) to the stack
+  call print_string
+  mov dx, global_descriptor_table
+  call print_hex_rm
+  mov dx, sd_null
+  call print_hex_rm
+
+global_descriptor_table:                   ; the gdt is a data structure used to define memory segments in x86 32 bit protected mode(pm)
+                                           ; an entry in the table is called segment descriptor(sd) and has a size of 8 bytes
+                                           ; the segment registers are used to point to these table entries => in pm segment registers store the index of a sd
+                                           ; a sd consists of a 32 bit base address that defines where the segment begins in memory,
+                                           ; a 20 bit segment limit which defines the size of the segment and various flags e.g. defining priviledge level or read/write permissions
+                                           ; for some historic reason the base address and limit bits are split into fragments, for the full sd structure see ./segment_descriptor.png
+                                           ; the first sd of the gdt must be an invalid null descriptor with all 8 bytes set to zero
+                                           ; this is done to catch errors when attempting to access an address through a misconfigured segment register 
+                                           ; => if an addressing attempt is made with the null descriptor the cpu raises an error/interrupt
+                                           ; e.g we had ds set to 0x0000 in rm and forgot to point it to an sd before switching to pm
+  sd_null:                                 
+    times 8 db 0x0                         
+  sd_code:                                 
+    times 2 db 0xff                        ; limit (bits 0-15 of first 4 bytes), for the exact meaning of sd fields and flags see ./segment_descriptor_fields.png
+    times 2 db 0x0                         ; base (bits 16-31 of first 4 bytes)
+    db 0x0                                 ; base (bits 0-7 of second 4 bytes)
+    db 10011010b                           ; 1st flags , type flags
+    db 11001111b                           ; 2nd flags , limit (bits 16-19 of second 4 bytes)
+    db 0x0                                 ; base (bits 24-31 of second 4 bytes)
     
-loop:                                      ; jumping to a label is done using a relative jump which means we do not need to know the specific address referenced by the loop label
-  jmp loop                                 ; instead begining at the byte immediatley after the jmp instruction, we calculate the distance in bytes to the label 
-                                           ; The first byte of a SHORT Jump is always EB and the second is a relative offset from 00h to 7Fh for Forward jumps
-                                           ; and from 80h to FFh for Reverse (or Backward) jumps https://thestarman.pcministry.com/asm/2bytejumps.htm 
+loop:
+  jmp loop
        
 print_string:                              ; this function takes one parameter that must be stored in bx before calling => bx should point to the starting address of a string
-  pusha                                    ; push all current register values to the stack => save the state of all registers before running this function
+  pusha
   mov al, [ds:bx]                          ; move the value stored at the address stored in bx to al (basically dereferencing)
   print_next_char:       
     call print_char       
@@ -27,8 +50,8 @@ print_string:                              ; this function takes one parameter t
     mov al, [ds:bx]       
     cmp al, 0x0                            ; check if the value of the char is zero, the string terminating character by convention
     jne print_next_char                    ; if the value is not zero, continue printing
-  popa                                     ; restore all registers to their state prior to running this function
-  ret                                      ; pop the return address off the stack and jmp to it / set instruction pointer to it
+  popa
+  ret
        
 print_hex_rm:                              ; this function takes a hex number as parameter that must be stored in dx before calling 
   pusha       
@@ -64,17 +87,16 @@ print_hex_nibble:                          ; this funciton takes one nibble (or 
     ret       
        
 print_char:                                ; this function takes one parameter that must be stored in al before calling => the value stored in al will be printed to the screen
-  pusha       
-  mov ah, 0xe                              ; if register ah is set to 0xe
-  int 0x10                                 ; the BIOS video interrupt int 0x10 triggers a scrolling teletype BIOS routine
-  popa       
-  ret       
+  pusha
+  mov ah, 0xe
+  int 0x10
+  popa
+  ret
        
 welcome_string:
   db 'Welcome to Davidos!', 0xA, 0xD, 0x0  ; write bytes to memory, quotes are syntactic sugar => db 'abc', 0x0 and db 'a', 'b', 'c', 0x0 are equivalent
        
 padding:       
-  times 510-(padding-main) db 0            ; to make the BIOS recognize this sector as a boot block we must ensure it has a size of exactly 512 bytes
-                                           ; => if the programm size (address of main up to address of padding) is smaller than 510 bytes we have to pad the remaining bytes with zeroes
+  times 510-(padding-main) db 0x0
        
-dw 0xaa55                                  ; the last 2 bytes must be the magic number 0xaa55 representing endianness
+dw 0xaa55
